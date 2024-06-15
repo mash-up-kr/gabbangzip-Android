e7757eb85589fc7f3b16346ef8f06e7c157c8c1c
JeonK1 <kan5016@naver.com>
2024-06-16 00:40:58 +0900

    👷 : ktlint conflict 수정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/MainActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/MainActivity.kt
@@ -3,8 +3,8 @@ package com.mashup.gabbangzip.sharedalbum.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.mashup.gabbangzip.sharedalbum.presentation.main.navigation.MainNavigator
 import com.mashup.gabbangzip.sharedalbum.presentation.main.MainScreen
+import com.mashup.gabbangzip.sharedalbum.presentation.main.navigation.MainNavigator
 import com.mashup.gabbangzip.sharedalbum.presentation.main.navigation.rememberMainNavigator
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
 import dagger.hilt.android.AndroidEntryPoint
