24ee2b8786a2be213849aa7b31a5b73d1c2dd715
JeonK1 <kan5016@naver.com>
2024-06-16 00:35:52 +0900

    ✅ : TopBar Preview 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/common/topbar/TopBar.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/common/topbar/TopBar.kt
@@ -2,6 +2,10 @@ package com.mashup.gabbangzip.sharedalbum.presentation.common.topbar
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.mashup.gabbangzip.sharedalbum.presentation.R
 import com.mashup.gabbangzip.sharedalbum.presentation.common.topbar.type.TopBarMain
 import com.mashup.gabbangzip.sharedalbum.presentation.common.topbar.type.TopBarProgress
 import com.mashup.gabbangzip.sharedalbum.presentation.common.topbar.type.TopBarTitle
@@ -32,4 +36,23 @@ fun TopBar(
             progress = topBarState.progress,
         )
     }
+}
+
+class TopBarProvider : PreviewParameterProvider<TopBarState> {
+    override val values: Sequence<TopBarState> = sequenceOf(
+        TopBarState.Title(titleText = "제목만 있는 탑바"),
+        TopBarState.Main(titleText = "제목만 있는 탑바", iconRes = R.drawable.ic_launcher_foreground),
+        TopBarState.Progress(titleText = "제목만 있는 탑바", max = 3, progress = 1),
+    )
+}
+
+@Preview
+@Composable
+fun TopBarPreview(
+    @PreviewParameter(TopBarProvider::class) state: TopBarState,
+) {
+    TopBar(
+        modifier = Modifier,
+        topBarState = state,
+    )
 }
