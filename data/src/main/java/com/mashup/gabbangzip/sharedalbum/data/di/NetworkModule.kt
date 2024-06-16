6e08f527237c06605ab77c4c36f710e57f3a749d
Jihye Joo <oreocube13@gmail.com>
2024-06-16 13:33:51 +0900

    :sparkles: 네트워크 모듈 구현

---
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.mashup.gabbangzip.sharedalbum.data.di
+
+import com.mashup.gabbangzip.sharedalbum.data.BuildConfig
+import com.mashup.gabbangzip.sharedalbum.data.service.LoginService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideLoginService(retrofit: Retrofit): LoginService = retrofit.create()
+
+    companion object {
+        private const val BASE_URL = "http://3.39.133.214:8080" // TODO("임시서버")
+    }
+}
