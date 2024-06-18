bd499d5434af064196c185e4fe03501c94c8aa6a
Jihye Joo <oreocube13@gmail.com>
2024-06-18 20:58:16 +0900

    :fire: 테스트용 코드 제거

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/LoginRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.mashup.gabbangzip.sharedalbum.data.repository
 
-import android.util.Log
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.LoginRequest
 import com.mashup.gabbangzip.sharedalbum.data.service.LoginService
 import com.mashup.gabbangzip.sharedalbum.domain.repository.LoginRepository
 import com.mashup.gabbangzip.sharedalbum.domain.usecase.LoginParam
 import javax.inject.Inject
-
-private const val TAG = "LoginRepositoryImpl"
 
 class LoginRepositoryImpl @Inject constructor(
     private val loginService: LoginService,
@@ -19,8 +16,6 @@ class LoginRepositoryImpl @Inject constructor(
             nickname = param.nickname,
             profileImage = param.profileImage,
         )
-        loginService.login(loginRequest = request).also {
-            Log.d(TAG, "login: $it")
-        }
+        loginService.login(loginRequest = request)
     }
 }
