58857440fcaafe73865ac305757e026d5559b41b
Jihye Joo <oreocube13@gmail.com>
2024-06-24 08:36:32 +0900

    :heavy_plus_sign: #26 lifecycle runtime 의존성 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.compose.material3)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
