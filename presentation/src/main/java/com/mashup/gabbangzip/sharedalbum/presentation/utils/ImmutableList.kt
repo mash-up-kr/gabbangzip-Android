c51542c290e9123e0fba3a42f18347102f81a92a
seojeongwoo <qw4643987@naver.com>
2024-06-25 13:41:41 +0900

    :hammer: ImmutableList 래퍼 클래스 by로 코드 수정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/utils/ImmutableList.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/utils/ImmutableList.kt
@@ -3,4 +3,4 @@ package com.mashup.gabbangzip.sharedalbum.presentation.utils
 import androidx.compose.runtime.Immutable
 
 @Immutable
-class ImmutableList<T>(val list: List<T>)
+class ImmutableList<T>(private val list: List<T>) : List<T> by list
