f2c6f364ce3d3fe162382fca4f804a57a2b99eda
Jihye Joo <oreocube13@gmail.com>
2024-06-25 22:42:50 +0900

    :sparkles: #33 스낵바 출력 위치 지정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicSnackbar.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicSnackbar.kt
@@ -15,9 +15,11 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,7 +48,13 @@ enum class PicSnackbarType(
 fun PicSnackbarHost(
     state: SnackbarHostState,
 ) {
+    val configuration = LocalConfiguration.current
+    val snackbarPosition = remember(configuration) {
+        (configuration.screenHeightDp - 80).dp
+    }
+
     SnackbarHost(
+        modifier = Modifier.padding(bottom = snackbarPosition),
         hostState = state,
     ) { data ->
         PicSnackbar(
