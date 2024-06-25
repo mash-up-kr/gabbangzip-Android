3d50c825b1c6c6763cf060ba583621bbdd1636a8
seojeongwoo <qw4643987@naver.com>
2024-06-25 13:46:00 +0900

    :hammer: 컴포저블 인자 추가 및 클래스 이름 변경

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/PhotoCard.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/PhotoCard.kt
@@ -1,28 +1,24 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.vote
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.Photo
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.VoteResult
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteResult
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.rememberSwipeState
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.swiper
 
 @Composable
-fun VoteCard(
+fun PhotoCard(
     modifier: Modifier = Modifier,
-    onSwiped: (result: VoteResult, photo: Photo) -> Unit,
     photo: Photo,
+    content: @Composable () -> Unit,
+    onSwiped: (result: PhotoVoteResult, photo: Photo) -> Unit,
 ) {
     val swiped = remember { mutableStateOf(false) }
 
@@ -39,20 +35,16 @@ fun VoteCard(
                         state = swipeState,
                         onDragLiked = {
                             swiped.value = true
-                            onSwiped(VoteResult.LIKE, photo)
+                            onSwiped(PhotoVoteResult.LIKE, photo)
                         },
-                        onDragDisLiked = {
+                        onDragDisliked = {
                             swiped.value = true
-                            onSwiped(VoteResult.DISLIKE, photo)
+                            onSwiped(PhotoVoteResult.DISLIKE, photo)
                         },
-                    )
-                    .background(
-                        color = Color.Gray,
-                        shape = RoundedCornerShape(12.dp),
                     ),
                 contentAlignment = Alignment.Center,
             ) {
-                Text(text = photo.date)
+                content()
             }
         }
     }
