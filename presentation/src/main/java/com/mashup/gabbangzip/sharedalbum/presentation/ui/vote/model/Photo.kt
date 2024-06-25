3a0ddf82b417d119fcd5c04d7b2186d4edd37f00
seojeongwoo <qw4643987@naver.com>
2024-06-25 13:45:15 +0900

    :hammer: 임시데이터 제거, url 타입 변경

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/Photo.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/Photo.kt
@@ -2,23 +2,6 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model
 
 data class Photo(
     val id: Int,
-    val imageUrl: Int,
+    val imageUrl: String,
     val date: String,
-) {
-    companion object {
-        fun getTempList(): List<Photo> {
-            return listOf(
-                Photo(1, 1, "2021-09-01"),
-                Photo(2, 2, "2021-09-02"),
-                Photo(3, 3, "2021-09-03"),
-                Photo(4, 4, "2021-09-04"),
-                Photo(5, 5, "2021-09-05"),
-                Photo(6, 6, "2021-09-06"),
-                Photo(7, 7, "2021-09-07"),
-                Photo(8, 8, "2021-09-08"),
-                Photo(9, 9, "2021-09-09"),
-                Photo(10, 10, "2021-09-10"),
-            )
-        }
-    }
-}
+)
