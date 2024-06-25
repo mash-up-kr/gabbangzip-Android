da4f7bbc8f2852e5eeff76f8869cc2b985b12256
Jihye Joo <oreocube13@gmail.com>
2024-06-25 21:14:37 +0900

    :sparkles: #33 PicSnackbar 컴포저블 구현

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicSnackbar.kt
@@ -0,0 +1,68 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.common
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.gabbangzip.sharedalbum.presentation.R
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.BlackAlpha50
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray0
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
+
+@Composable
+fun PicSnackbar(
+    @DrawableRes iconResId: Int? = null,
+    message: String,
+) {
+    Row(
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(30.dp))
+            .background(color = BlackAlpha50)
+            .wrapContentSize()
+            .padding(
+                horizontal = 24.dp,
+                vertical = 20.dp,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (iconResId != null) {
+            Image(
+                modifier = Modifier.size(20.dp),
+                painter = painterResource(id = iconResId),
+                contentDescription = "snackbar icon",
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+        }
+        Text(
+            text = message,
+            color = Gray0,
+            style = PicTypography.bodyMedium16,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PickSnackbarPreView() {
+    SharedAlbumTheme {
+        PicSnackbar(
+            iconResId = R.drawable.ic_warning,
+            message = "로그인에 실패했어요.",
+        )
+    }
+}
