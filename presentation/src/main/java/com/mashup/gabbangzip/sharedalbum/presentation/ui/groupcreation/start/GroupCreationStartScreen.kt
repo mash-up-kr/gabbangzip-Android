a42c22f714345c0c6369da4310d5f9f87aff30e1
JeonK1 <kan5016@naver.com>
2024-06-27 00:54:45 +0900

    ✨ : Compose 코드 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/start/GroupCreationStartScreen.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/start/GroupCreationStartScreen.kt
@@ -1,30 +1,94 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.start
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.mashup.gabbangzip.sharedalbum.presentation.R
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.pretendard
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.start.component.BottomButton
 
 @Composable
 fun GroupCreationFirstScreen(
+    modifier: Modifier = Modifier,
     onClickNextButton: () -> Unit,
 ) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.moving_shape))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+    )
+
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            modifier = Modifier.wrapContentSize(),
-            text = "그룹만들기 화면입니다",
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(top = 84.dp),
+            text = stringResource(id = R.string.group_creation_title),
+            textAlign = TextAlign.Center,
+            color = Gray80,
+            letterSpacing = (0.02).em,
+            fontSize = 24.sp,
+            fontFamily = pretendard,
+            fontWeight = FontWeight.Bold,
         )
-        Button(onClick = onClickNextButton) {
-            Text(text = "다음 화면으로 이동하는 버튼")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+            LottieAnimation(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .align(Alignment.Center),
+                composition = composition,
+                progress = { progress },
+            )
+
         }
+        BottomButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(bottom = 16.dp, start = 22.dp, end = 22.dp),
+            buttonText = stringResource(id = R.string.group_creation_button_name),
+            descriptionText = stringResource(id = R.string.group_creation_button_description),
+            onClickNextButton = onClickNextButton,
+        )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GroupCreationFirstScreenPreview() {
+    GroupCreationFirstScreen(
+        onClickNextButton = {},
+    )
 }
---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/start/component/BottomButton.kt
@@ -0,0 +1,75 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.start.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray0
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray60
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.platformRippleClickable
+
+@Composable
+fun BottomButton(
+    modifier: Modifier = Modifier,
+    descriptionText: String,
+    buttonText: String,
+    onClickNextButton: () -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(bottom = 8.dp),
+            text = descriptionText,
+            textAlign = TextAlign.Center,
+            color = Gray60,
+            style = PicTypography.bodyMedium14,
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .clip(RoundedCornerShape(16.dp))
+                .background(Gray80)
+                .platformRippleClickable(onClick = onClickNextButton),
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(vertical = 20.dp),
+                text = buttonText,
+                textAlign = TextAlign.Center,
+                style = PicTypography.bodyMedium17,
+                color = Gray0,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomButtonPreview() {
+    BottomButton(
+        descriptionText = "초대 링크를 받으셨나요?\n링크를 눌러 바로 그룹에 들어갈 수 있어요.",
+        buttonText = "그룹 만들기",
+        onClickNextButton = {}
+    )
+}
