d2c5270d20fb45c37f462517e602aed643ced8e6
JeonK1 <kan5016@naver.com>
2024-06-28 21:52:21 +0900

    ✅ : Preview 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/mypage/component/GroupTitle.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/mypage/component/GroupTitle.kt
@@ -1,11 +1,9 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.mypage.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
 
@@ -20,4 +18,10 @@ fun GroupTitle(
         style = PicTypography.headBold14,
         color = Gray80,
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GroupTitlePreview() {
+    GroupTitle(text = "알림 설정")
 }
--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/mypage/component/UserContainer.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/mypage/component/UserContainer.kt
@@ -3,6 +3,7 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.mypage.component
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
 
@@ -17,4 +18,10 @@ fun UserContainer(
         style = PicTypography.headBold20,
         color = Gray80,
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserContainerPreview() {
+    UserContainer(userName = "계원")
 }
