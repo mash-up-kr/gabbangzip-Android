c72aa2bd65d0c183c2db0cc51481e430a0340104
seojeongwoo <qw4643987@naver.com>
2024-06-30 00:18:55 +0900

    :sparkles: 그룹 홈 uiState 생성

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouplist/model/GroupHomeUiState.kt
@@ -0,0 +1,11 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouplist.model
+
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupInfo
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.UserInfo
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
+
+data class GroupHomeUiState(
+    val groupList: ImmutableList<GroupInfo> = ImmutableList(emptyList()),
+    val user: UserInfo = UserInfo("", ""),
+    val isLoading: Boolean = true,
+)
