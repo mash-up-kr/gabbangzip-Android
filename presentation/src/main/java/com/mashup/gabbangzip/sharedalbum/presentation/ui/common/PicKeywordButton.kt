b1734cbd68cf9834e69a636f717c75625453a784
sooziini <soojinpar1026@gmail.com>
2024-06-29 14:43:21 +0900

    :recycle: #21 ktlint 에러 수정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicKeywordButton.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicKeywordButton.kt
@@ -1,7 +1,6 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.common
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
