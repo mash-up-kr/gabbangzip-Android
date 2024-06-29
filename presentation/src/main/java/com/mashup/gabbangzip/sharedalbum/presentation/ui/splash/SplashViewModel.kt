619ab38e75e1fe9812a2ae951fdc497d0019ba96
Jihye Joo <oreocube13@gmail.com>
2024-06-29 21:37:57 +0900

    :sparkles: #48 스플래시 디폴트 출력 시간 설정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/splash/SplashViewModel.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/splash/SplashViewModel.kt
@@ -1,11 +1,14 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mashup.gabbangzip.sharedalbum.domain.usecase.CheckUserLoggedInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,12 +22,19 @@ class SplashViewModel @Inject constructor(
         _state.update { state ->
             state.copy(isLoading = true)
         }
-        val isUserLoggedIn = checkUserLoggedInUseCase()
-        _state.update { state ->
-            state.copy(
-                isUserLoggedIn = isUserLoggedIn,
-                isLoading = false,
-            )
+        viewModelScope.launch {
+            val isUserLoggedIn = checkUserLoggedInUseCase()
+            delay(SPLASH_DISPLAY_TIME)
+            _state.update { state ->
+                state.copy(
+                    isUserLoggedIn = isUserLoggedIn,
+                    isLoading = false,
+                )
+            }
         }
+    }
+
+    companion object {
+        private const val SPLASH_DISPLAY_TIME = 3000L
     }
 }
