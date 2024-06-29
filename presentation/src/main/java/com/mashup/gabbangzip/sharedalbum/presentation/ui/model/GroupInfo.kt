128df9616cb88b9b6aafaf4be176199ed58bcf4b
seojeongwoo <qw4643987@naver.com>
2024-06-30 00:16:42 +0900

    :sparkles: 그룹 정보 모델 생성

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupInfo.kt
@@ -0,0 +1,15 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.model
+
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
+
+data class GroupInfo(
+    val id: Int,
+    val date: String,
+    val groupName: String,
+    val eventName: String,
+    val thumbnailUrl: String,
+    val thumbnailFrameUrl: String,
+    val fourCutImageUrl: String,
+    val keyword: GroupKeyWord,
+    val tags: ImmutableList<String>,
+)
