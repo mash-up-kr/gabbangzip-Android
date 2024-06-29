62848e07315640a0ed1b5c1b748bf4ad17dc76e1
sooziini <soojinpar1026@gmail.com>
2024-06-29 20:23:16 +0900

    :sparkles: #44 PicTextField 컴포넌트 구현

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicTextField.kt
@@ -0,0 +1,82 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Cultured
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.SilverSand
+
+@Composable
+fun PicTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+    maxLength: Int = 0,
+) {
+    BasicTextField(
+        modifier = modifier
+            .background(
+                color = Cultured,
+                shape = RoundedCornerShape(10.dp),
+            )
+            .padding(horizontal = 20.dp, vertical = 18.dp),
+        value = value,
+        onValueChange = {
+            if (maxLength == 0 || it.length <= maxLength) {
+                onValueChange(it)
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+        ),
+        textStyle = PicTypography.bodyMedium16.copy(color = Gray80),
+        maxLines = 1,
+        cursorBrush = SolidColor(Gray80),
+        decorationBox = { innerTextField ->
+            if (value.isEmpty()) {
+                Text(
+                    text = hint,
+                    style = PicTypography.bodyMedium16,
+                    color = SilverSand,
+                )
+            }
+            innerTextField()
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PicTextFieldPreview() {
+    SharedAlbumTheme {
+        Column(modifier = Modifier.background(Gray80)) {
+            var text by remember { mutableStateOf("") }
+            PicTextField(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                value = text,
+                onValueChange = { text = it },
+                hint = "최대 10자 까지 입력 가능해요.",
+                maxLength = 10,
+            )
+        }
+    }
+}
