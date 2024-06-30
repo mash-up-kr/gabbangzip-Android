e592a5deda92c5daa1aaa57f74a2fe919dc05d4c
sooziini <soojinpar1026@gmail.com>
2024-06-30 20:36:45 +0900

    :sparkles: #8 GroupCreationActivity Surface -> Scaffold 변경

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/GroupCreationActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/GroupCreationActivity.kt
@@ -5,11 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray0
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.navigation.GroupCreationNavHost
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.navigation.GroupCreationRoute
@@ -21,15 +24,19 @@ class GroupCreationActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SharedAlbumTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    GroupCreationNavHost(
-                        modifier = Modifier.fillMaxSize(),
-                        navController = rememberNavController(),
-                        startDestination = GroupCreationRoute.initRoute,
-                    )
+                Scaffold { contentPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Gray0)
+                            .padding(contentPadding)
+                    ) {
+                        GroupCreationNavHost(
+                            modifier = Modifier.fillMaxSize(),
+                            navController = rememberNavController(),
+                            startDestination = GroupCreationRoute.initRoute,
+                        )
+                    }
                 }
             }
         }
