b77c797a0b26f9ebdd3f0a2f94e0e6f0e78a9b20
seojeongwoo <qw4643987@naver.com>
2024-07-02 23:18:07 +0900

    :sparkles: 태그 stringResId 인자 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
@@ -1,6 +1,7 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.model
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import com.mashup.gabbangzip.sharedalbum.presentation.R
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.ConiferAlpha30
@@ -14,34 +15,49 @@ import com.mashup.gabbangzip.sharedalbum.presentation.theme.MayaBlueAlpha30
 enum class GroupKeyword(
     @DrawableRes val symbolResId: Int,
     val backgroundColor: Color,
+    @StringRes val tagNameResId: Int,
 ) {
     SCHOOL(
         symbolResId = R.drawable.sb_school,
         backgroundColor = ConiferAlpha30,
+        tagNameResId = R.string.tag_school,
     ),
     COMPANY(
         symbolResId = R.drawable.sb_company,
         backgroundColor = MagentaPinkAlpha30,
+        tagNameResId = R.string.tag_company,
     ),
     CREW(
         symbolResId = R.drawable.sb_crew,
         backgroundColor = MayaBlueAlpha30,
+        tagNameResId = R.string.tag_crew,
     ),
     NETWORK(
         symbolResId = R.drawable.sb_network,
         backgroundColor = CoralAlpha30,
+        tagNameResId = R.string.tag_network,
     ),
     EXERCISE(
         symbolResId = R.drawable.sb_exercise,
         backgroundColor = DandelionAlpha30,
+        tagNameResId = R.string.tag_exercise,
     ),
     HOBBY(
         symbolResId = R.drawable.sb_hobby,
         backgroundColor = MalibuAlpha30,
+        tagNameResId = R.string.tag_hobby,
     ),
     LITTLE_MOIM(
         symbolResId = R.drawable.sb_little_moim,
         backgroundColor = LavenderAlpha30,
+        tagNameResId = R.string.tag_somoim,
     ),
     ;
+
+    companion object {
+        fun getKeyword(keyword: String): GroupKeyWord {
+            return entries.associateBy { it.name }[keyword]
+                ?: throw IllegalArgumentException("Invalid keyword")
+        }
+    }
 }
