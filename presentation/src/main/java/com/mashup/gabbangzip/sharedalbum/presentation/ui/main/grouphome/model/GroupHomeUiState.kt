c95d25edf043d7cd814e6c884feef1df5f131507
seojeongwoo <qw4643987@naver.com>
2024-07-02 23:19:15 +0900

    :fire: userInfo 모델 코드 삭제

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/model/GroupHomeUiState.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/model/GroupHomeUiState.kt
@@ -1,11 +1,8 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model
 
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupInfo
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.UserInfo
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
 
 data class GroupHomeUiState(
     val groupList: ImmutableList<GroupInfo> = ImmutableList(emptyList()),
-    val user: UserInfo = UserInfo("", ""),
     val isLoading: Boolean = false,
 )
