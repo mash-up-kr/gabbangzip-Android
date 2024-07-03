b2595f8896654b917fb0b669351b80884550afdf
sooziini <soojinpar1026@gmail.com>
2024-07-03 23:24:21 +0900

    :sparkles: #9 GroupCreationKeywordScreen 구현

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/keyword/GroupCreationKeywordScreen.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/keyword/GroupCreationKeywordScreen.kt
@@ -1,20 +1,20 @@
-package com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.name
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.keyword
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,22 +25,20 @@ import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicBackButtonTopBar
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicButton
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicKeywordButton
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicProgressBar
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicTextField
-import com.mashup.gabbangzip.sharedalbum.presentation.utils.hideKeyboardOnOutsideClicked
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupKeyWord
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun GroupCreationNameScreen(
+fun GroupCreationKeywordScreen(
     onBackButtonClicked: () -> Unit,
     onNextButtonClicked: () -> Unit,
 ) {
-    val focusManager = LocalFocusManager.current
-    var name by remember { mutableStateOf("") }
-    val buttonEnabled by remember { derivedStateOf { name.isNotBlank() } }
+    val scrollState = rememberScrollState()
+    val (selectedKeyword, setSelected) =  remember { mutableStateOf(GroupKeyWord.SCHOOL) }
 
-    Column(
-        modifier = Modifier.hideKeyboardOnOutsideClicked(),
-    ) {
+    Column {
         PicBackButtonTopBar(
             modifier = Modifier.background(Gray0.copy(alpha = 0.2f)).padding(top = 8.dp),
             titleText = stringResource(id = R.string.group_creation_button_name),
@@ -55,49 +53,51 @@ fun GroupCreationNameScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 32.dp),
-                level = 1,
+                level = 2,
                 total = 4f,
             )
             Text(
-                modifier = Modifier.padding(bottom = 16.dp),
-                text = stringResource(id = R.string.group_creation_name_title),
+                modifier = Modifier.padding(bottom = 24.dp),
+                text = stringResource(id = R.string.group_creation_keyword_title),
                 style = PicTypography.headBold18,
                 color = Gray80,
             )
-            PicTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = name,
-                onValueChange = { name = it },
-                hint = stringResource(id = R.string.group_creation_name_hint),
-                maxLength = 10,
-            )
+            FlowRow(
+                modifier = Modifier.verticalScroll(scrollState),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                GroupKeyWord.entries.forEach { groupKeyword ->
+                    PicKeywordButton(
+                        keyword = groupKeyword,
+                        selected = selectedKeyword == groupKeyword,
+                        onButtonClicked = { keyword -> setSelected(keyword) },
+                    )
+                }
+            }
         }
         PicButton(
             modifier = Modifier
                 .fillMaxWidth()
-                .imePadding()
                 .padding(start = 22.dp, end = 22.dp, bottom = 16.dp),
             text = stringResource(id = R.string.group_creation_button_next),
             isRippleClickable = true,
-            enable = buttonEnabled,
-            onButtonClicked = {
-                focusManager.clearFocus()
-                onNextButtonClicked()
-            },
+            enable = true,
+            onButtonClicked = onNextButtonClicked,
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GroupCreationNameScreenPreview() {
+fun GroupCreationKeywordScreenPreview() {
     SharedAlbumTheme {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Gray0),
         ) {
-            GroupCreationNameScreen(
+            GroupCreationKeywordScreen(
                 onBackButtonClicked = {},
                 onNextButtonClicked = {},
             )
