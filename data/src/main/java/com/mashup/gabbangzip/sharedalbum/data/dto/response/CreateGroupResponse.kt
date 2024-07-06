64218e646af9fc626d089be1f8584c3f2d46fd71
JeonK1 <kan5016@naver.com>
2024-07-06 14:11:20 +0900

    🔥 : Domain Layer 의 GroupKeyword 제거 후 String 으로 전환

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/dto/response/CreateGroupResponse.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/dto/response/CreateGroupResponse.kt
@@ -1,6 +1,5 @@
 package com.mashup.gabbangzip.sharedalbum.data.dto.response
 
-import com.mashup.gabbangzip.sharedalbum.domain.model.GroupKeyword
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -11,7 +10,7 @@ data class CreateGroupResponse(
     @Json(name = "group_name")
     val groupName: String,
     @Json(name = "keyword")
-    val keyword: GroupKeyword?,
+    val keyword: String,
     @Json(name = "group_image_url")
     val groupImageUrl: String,
     @Json(name = "group_invitation_url")
--- domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/model/GroupInfo.kt
+++ domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/model/GroupInfo.kt
@@ -3,7 +3,7 @@ package com.mashup.gabbangzip.sharedalbum.domain.model
 data class GroupInfo(
     val id: Long,
     val name: String,
-    val keyword: GroupKeyword?,
+    val keyword: String,
     val imageUrl: String,
     val invitationUrl: String,
 )
