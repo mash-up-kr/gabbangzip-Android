f6b2afe97cf0f81a02a6ce0f5626aeeea242b021
seojeongwoo <qw4643987@naver.com>
2024-07-09 16:44:58 +0900

    :sparkles: 사진 프레임 타입 변경

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/model/CardBackImage.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/model/CardBackImage.kt
@@ -1,15 +1,16 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model
 
 import com.mashup.gabbangzip.sharedalbum.domain.model.group.CardBackImageDomainModel
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.PicPhotoFrame
 
 data class CardBackImage(
-    val frameType: String,
+    val frameType: PicPhotoFrame,
     val imageUrl: String,
 )
 
 fun CardBackImageDomainModel.toUiModel(): CardBackImage {
     return CardBackImage(
-        frameType = frameType,
+        frameType = PicPhotoFrame.getType(frameType),
         imageUrl = imageUrl,
     )
 }
