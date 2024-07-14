de22794e19bb86e60dbcbe2a96c457a51f829a3c
Jihye Joo <oreocube13@gmail.com>
2024-07-14 18:27:53 +0900

    :sparkles: GroupDetailViewModel 연결

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailScreen.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -42,6 +43,7 @@ fun GroupDetailScreen(
     groupId: Long,
     onClickGroupMemberButton: () -> Unit,
     onClickBackButton: () -> Unit,
+    viewModel: GroupDetailViewModel = hiltViewModel(),
 ) {
     // TODO: viewModel state 연결
 }
@@ -60,7 +62,7 @@ fun GroupDetailScreen(
     ) {
         PicBackButtonTopBar(
             modifier = Modifier.fillMaxWidth(),
-            titleText = state.groupInfo.name,
+            titleText = state.groupInfo?.name.orEmpty(),
             titleAlign = PicTopBarTitleAlign.LEFT,
             backButtonClicked = onClickBackButton,
             rightIcon1 = PicTopBarIcon.PLUS,
--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/GroupDetailUiState.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/GroupDetailUiState.kt
@@ -3,7 +3,9 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail.model
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.GroupInfo
 
 data class GroupDetailUiState(
-    val groupInfo: GroupInfo,
+    val isLoading: Boolean = false,
+    val groupInfo: GroupInfo? = null,
     val recentEvent: GroupEvent? = null,
-    val history: List<HistoryItem>,
+    val history: List<HistoryItem> = emptyList(),
+    val error: String? = null,
 )
