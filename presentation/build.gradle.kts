c0fa58b577be0abca10fa5d02b4e61a370031b5e
Jihye Joo <oreocube13@gmail.com>
2024-07-14 19:20:15 +0900

    :heavy_plus_sign: #106 androidx material 의존성 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.compose.material3)
+    implementation(libs.androidx.material3.android)
     implementation(libs.androidx.lifecycle.runtime.compose.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
