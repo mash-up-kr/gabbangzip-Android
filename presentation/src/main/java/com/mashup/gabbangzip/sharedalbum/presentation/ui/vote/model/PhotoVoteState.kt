b3db55a7171dcbbee3f5c58263dda1eaa5eb502c
seojeongwoo <qw4643987@naver.com>
2024-07-22 17:03:52 +0900

    :sparkles: 모델에 초기값 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/PhotoVoteState.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/PhotoVoteState.kt
@@ -4,7 +4,9 @@ import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.UserInfo
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
 
 data class PhotoVoteState(
-    val photoList: ImmutableList<Photo>,
-    val userInfo: UserInfo,
-    val isLoading: Boolean,
+    val photoList: ImmutableList<Photo> = ImmutableList(emptyList()),
+    val userInfo: UserInfo = UserInfo(),
+    val voteClickInfo: VoteClickInfo = VoteClickInfo(),
+    val isVoteCancel: Boolean = false,
+    val isLoading: Boolean = false,
 )
