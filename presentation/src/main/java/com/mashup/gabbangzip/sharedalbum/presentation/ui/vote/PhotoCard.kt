4c47d945508746e661a309d78d5fd87b680f713b
seojeongwoo <qw4643987@naver.com>
2024-07-22 16:57:15 +0900

    :sparkles: 클릭시 카드 스와이프 되는 기능 구현

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/PhotoCard.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/PhotoCard.kt
@@ -1,26 +1,40 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.vote
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.contract.VoteConstant
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.Photo
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteResult
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteType
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.VoteClickInfo
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.rememberSwipeState
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.swiper
+import kotlinx.coroutines.delay
 
 @Composable
 fun PhotoCard(
     modifier: Modifier = Modifier,
     photo: Photo,
+    voteClickInfo: VoteClickInfo,
+    currentIndex: Int,
+    onSwiped: (result: PhotoVoteType, photo: Photo) -> Unit,
     content: @Composable () -> Unit,
-    onSwiped: (result: PhotoVoteResult, photo: Photo) -> Unit,
+    onVoteClickSwiped: (result: PhotoVoteType, photo: Photo) -> Unit,
 ) {
     val swiped = remember { mutableStateOf(false) }
+    val isSwiped = remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
 
     BoxWithConstraints(modifier = modifier) {
         val swipeState = rememberSwipeState(
@@ -35,17 +49,58 @@ fun PhotoCard(
                         state = swipeState,
                         onDragLiked = {
                             swiped.value = true
-                            onSwiped(PhotoVoteResult.LIKE, photo)
+                            onSwiped(PhotoVoteType.LIKE, photo)
                         },
                         onDragDisliked = {
                             swiped.value = true
-                            onSwiped(PhotoVoteResult.DISLIKE, photo)
+                            onSwiped(PhotoVoteType.DISLIKE, photo)
                         },
                     ),
                 contentAlignment = Alignment.Center,
             ) {
                 content()
             }
+        }
+
+        LaunchedEffect(voteClickInfo.index == currentIndex) {
+            when (voteClickInfo.type) {
+                PhotoVoteType.LIKE -> {
+                    isSwiped.value = true
+                    onVoteClickSwiped(PhotoVoteType.LIKE, photo)
+                    swipeState.liked(coroutineScope)
+                    delay(VoteConstant.CARD_DELAY_TIME)
+                    swiped.value = true
+                    isSwiped.value = false
+                }
+
+                PhotoVoteType.DISLIKE -> {
+                    isSwiped.value = true
+                    onVoteClickSwiped(PhotoVoteType.DISLIKE, photo)
+                    swipeState.disliked(coroutineScope)
+                    delay(VoteConstant.CARD_DELAY_TIME)
+                    swiped.value = true
+                    isSwiped.value = false
+                }
+
+                else -> Unit
+            }
+        }
+
+        // 버튼 클릭후 1초 딜레이 되는동안 카드 스와이프 막기 위함
+        if (isSwiped.value) {
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(color = Color.Transparent)
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragStart = { },
+                            onDrag = { _, _ -> },
+                            onDragEnd = { },
+                            onDragCancel = { },
+                        )
+                    },
+            )
         }
     }
 }
