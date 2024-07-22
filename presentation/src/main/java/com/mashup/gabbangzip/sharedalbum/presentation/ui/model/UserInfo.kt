f740d7bc32b1aa5f9f30fab836c064191056f62e
seojeongwoo <qw4643987@naver.com>
2024-07-22 17:03:26 +0900

    :sparkles: userinfo 초기값 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/UserInfo.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/UserInfo.kt
@@ -1,6 +1,6 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.model
 
 data class UserInfo(
-    val imageUrl: String,
-    val name: String,
+    val imageUrl: String = "",
+    val name: String = "",
 )
