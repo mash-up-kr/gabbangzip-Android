cfdce444e6347547ff6fbfd0013419d6cae2869f
JeonK1 <kan5016@naver.com>
2024-07-26 21:02:16 +0900

    ✨ : GroupKeyword 에 Frame 정보 연결

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
@@ -24,48 +24,56 @@ enum class GroupKeyword(
     val backgroundColor: Color,
     val symbolColor: Color,
     @StringRes val tagNameResId: Int,
+    val frame: PicPhotoFrame,
 ) {
     SCHOOL(
         symbolResId = R.drawable.sb_school,
         backgroundColor = ConiferAlpha30,
         symbolColor = Conifer,
         tagNameResId = R.string.tag_school,
+        frame = PicPhotoFrame.SNOWMAN,
     ),
     CREW(
         symbolResId = R.drawable.sb_crew,
         backgroundColor = MayaBlueAlpha30,
         symbolColor = MayaBlue,
         tagNameResId = R.string.tag_crew,
+        frame = PicPhotoFrame.HAMBURGER,
     ),
     COMPANY(
         symbolResId = R.drawable.sb_company,
         backgroundColor = MagentaPinkAlpha30,
         symbolColor = MagentaPink,
         tagNameResId = R.string.tag_company,
+        frame = PicPhotoFrame.GHOST,
     ),
     LITTLE_MOIM(
         symbolResId = R.drawable.sb_little_moim,
         backgroundColor = LavenderAlpha30,
         symbolColor = Lavender,
         tagNameResId = R.string.tag_somoim,
+        frame = PicPhotoFrame.CLOVER,
     ),
     NETWORK(
         symbolResId = R.drawable.sb_network,
         backgroundColor = CoralAlpha30,
         symbolColor = Coral,
         tagNameResId = R.string.tag_network,
+        frame = PicPhotoFrame.PLUS,
     ),
     EXERCISE(
         symbolResId = R.drawable.sb_exercise,
         backgroundColor = DandelionAlpha30,
         symbolColor = Dandelion,
         tagNameResId = R.string.tag_exercise,
+        frame = PicPhotoFrame.SEXY,
     ),
     HOBBY(
         symbolResId = R.drawable.sb_hobby,
         backgroundColor = MalibuAlpha30,
         symbolColor = Malibu,
         tagNameResId = R.string.tag_hobby,
+        frame = PicPhotoFrame.FLOWER,
     ),
     ;
 
