8feeab00e9bbb1d106e165c4fb2c880bd27da821
JeonK1 <kan5016@naver.com>
2024-07-27 14:43:58 +0900

    ♻️ : bukcet 주소 별도 리소스로 분리

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/GroupRepositoryImpl.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/GroupRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.mashup.gabbangzip.sharedalbum.data.repository
 import com.mashup.gabbangzip.sharedalbum.data.base.callApi
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.CreateGroupRequest
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.group.toDomainModel
+import com.mashup.gabbangzip.sharedalbum.data.service.AwsService
 import com.mashup.gabbangzip.sharedalbum.data.service.GroupService
 import com.mashup.gabbangzip.sharedalbum.domain.model.GroupParam
 import com.mashup.gabbangzip.sharedalbum.domain.model.group.GroupDomainModel
@@ -25,7 +26,7 @@ class GroupRepositoryImpl @Inject constructor(
                 id = id,
                 name = groupName,
                 keyword = keyword,
-                imageUrl = "https://pic-api-bucket.s3.ap-northeast-2.amazonaws.com/$groupImageUrl",
+                imageUrl = "${AwsService.BASE_URL}$groupImageUrl",
                 invitationUrl = groupInvitationUrl ?: "", // Todo : 초대코드 대응하기
             )
         }
