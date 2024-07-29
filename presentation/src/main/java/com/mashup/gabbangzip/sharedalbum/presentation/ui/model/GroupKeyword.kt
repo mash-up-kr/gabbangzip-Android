c5d0b7b6bcf61664e80b7de57d077057fa82d544
seojeongwoo <qw4643987@naver.com>
2024-07-29 17:43:17 +0900

    :sparkles: 카드 뒷면 색상 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/GroupKeyword.kt
@@ -5,24 +5,31 @@ import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import com.mashup.gabbangzip.sharedalbum.presentation.R
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Conifer
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.ConiferAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.ConiferAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.ConiferAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Coral
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.CoralAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.CoralAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.CoralAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Dandelion
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.DandelionAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.DandelionAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.DandelionAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Lavender
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.LavenderAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.LavenderAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.LavenderAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MagentaPink
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.MagentaPinkAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MagentaPinkAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MagentaPinkAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Malibu
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.MalibuAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MalibuAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MalibuAlpha40
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MayaBlue
+import com.mashup.gabbangzip.sharedalbum.presentation.theme.MayaBlueAlpha20
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MayaBlueAlpha30
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.MayaBlueAlpha40
 
@@ -32,6 +39,7 @@ enum class GroupKeyword(
     val frameInnerColor: Color,
     val symbolColor: Color,
     @StringRes val tagNameResId: Int,
+    val backBackGroundColor: Color,
 ) {
     SCHOOL(
         symbolResId = R.drawable.sb_school,
@@ -39,6 +47,7 @@ enum class GroupKeyword(
         frameInnerColor = ConiferAlpha40,
         symbolColor = Conifer,
         tagNameResId = R.string.tag_school,
+        backBackGroundColor = ConiferAlpha20,
     ),
     CREW(
         symbolResId = R.drawable.sb_crew,
@@ -46,6 +55,7 @@ enum class GroupKeyword(
         frameInnerColor = MayaBlueAlpha40,
         symbolColor = MayaBlue,
         tagNameResId = R.string.tag_crew,
+        backBackGroundColor = MayaBlueAlpha20,
     ),
     COMPANY(
         symbolResId = R.drawable.sb_company,
@@ -53,6 +63,7 @@ enum class GroupKeyword(
         frameInnerColor = MagentaPinkAlpha40,
         symbolColor = MagentaPink,
         tagNameResId = R.string.tag_company,
+        backBackGroundColor = MagentaPinkAlpha20,
     ),
     LITTLE_MOIM(
         symbolResId = R.drawable.sb_little_moim,
@@ -60,6 +71,7 @@ enum class GroupKeyword(
         frameInnerColor = LavenderAlpha40,
         symbolColor = Lavender,
         tagNameResId = R.string.tag_somoim,
+        backBackGroundColor = LavenderAlpha20,
     ),
     NETWORK(
         symbolResId = R.drawable.sb_network,
@@ -67,6 +79,7 @@ enum class GroupKeyword(
         frameInnerColor = CoralAlpha40,
         symbolColor = Coral,
         tagNameResId = R.string.tag_network,
+        backBackGroundColor = CoralAlpha20,
     ),
     EXERCISE(
         symbolResId = R.drawable.sb_exercise,
@@ -74,6 +87,7 @@ enum class GroupKeyword(
         frameInnerColor = DandelionAlpha40,
         symbolColor = Dandelion,
         tagNameResId = R.string.tag_exercise,
+        backBackGroundColor = DandelionAlpha20,
     ),
     HOBBY(
         symbolResId = R.drawable.sb_hobby,
@@ -81,6 +95,7 @@ enum class GroupKeyword(
         frameInnerColor = MalibuAlpha40,
         symbolColor = Malibu,
         tagNameResId = R.string.tag_hobby,
+        backBackGroundColor = MalibuAlpha20,
     ),
     ;
 
