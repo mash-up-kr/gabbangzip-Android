d10861cfba883fabb948f1f7f43aa5f26d811a12
seojeongwoo <qw4643987@naver.com>
2024-07-29 17:42:59 +0900

    :rewind: 원래 코드로 롤백

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicPhotoCard.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicPhotoCard.kt
@@ -3,19 +3,12 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.common
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,93 +21,35 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.mashup.gabbangzip.sharedalbum.presentation.R
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray0
-import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
-import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.CardBackImage
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.GroupInfo
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupKeyword
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupStatusType
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.PicPhotoFrame
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.StableImage
-import com.mashup.gabbangzip.sharedalbum.presentation.utils.rippleClickable
 
 @Composable
 fun PicPhotoCard(
     modifier: Modifier,
     groupInfo: GroupInfo,
-    content: @Composable BoxScope.() -> Unit,
-    eventName: String = "",
-    onCreateEvent: () -> Unit = {},
 ) {
-    PicPhotoCardContainer(
+    PicPhotoCardFrame(
         modifier = modifier,
         keywordType = groupInfo.keyword,
+        frameResId = groupInfo.frontImageFrame.frameResId,
     ) {
-        if (groupInfo.status == GroupStatusType.NO_PAST_AND_CURRENT_EVENT) {
-            CardTopTitle(
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 37.dp),
-                text = "이벤트를 만들어보세요",
-            )
-        }
-
-        if (groupInfo.status == GroupStatusType.NO_CURRENT_EVENT) {
-            CardTopTitle(
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = 37.dp),
-                text = groupInfo.recentEventDate,
-            )
-        }
-
-        if (groupInfo.status == GroupStatusType.NO_PAST_AND_CURRENT_EVENT) {
-            PicNormalButton(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(top = 19.dp, bottom = 34.dp)
-                    .rippleClickable { onCreateEvent() },
-                text = "이벤트 생성하기",
-            )
-        }
-
-        content()
-
-        if (groupInfo.status == GroupStatusType.NO_CURRENT_EVENT) {
-            EventTitle(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(top = 31.dp),
-                text = eventName,
-            )
-        }
+        AsyncImage(
+            modifier = Modifier.matchParentSize(),
+            model = groupInfo.cardFrontImageUrl,
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(R.string.group_main_picture),
+        )
     }
 }
 
 @Composable
-private fun CardTopTitle(modifier: Modifier, text: String) {
-    Text(
-        modifier = modifier,
-        text = text,
-        color = Gray80,
-        style = PicTypography.bodyMedium16,
-    )
-}
-
-@Composable
-private fun EventTitle(modifier: Modifier, text: String) {
-    Text(
-        modifier = modifier,
-        text = text,
-        color = Gray80,
-        style = PicTypography.headBold20,
-    )
-}
-
-@Composable
-fun PicPhotoCardContainer(
+fun PicPhotoCardFrame(
     modifier: Modifier,
     keywordType: GroupKeyword,
+    @DrawableRes frameResId: Int,
     content: @Composable BoxScope.() -> Unit,
 ) {
     Box(
@@ -129,8 +64,6 @@ fun PicPhotoCardContainer(
                 shape = RoundedCornerShape(20.dp),
             ),
     ) {
-        content()
-
         KeywordMiniSymbol(
             modifier = Modifier
                 .align(Alignment.TopStart)
@@ -170,6 +103,16 @@ fun PicPhotoCardContainer(
                 ),
             keyword = keywordType,
         )
+
+        PicCroppedImageContainer(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 74.dp, bottom = 96.dp, start = 30.dp, end = 30.dp)
+                .align(Alignment.Center),
+            frameResId = frameResId,
+            backgroundColor = keywordType.backgroundColor,
+            content = content,
+        )
     }
 }
 
@@ -183,21 +126,18 @@ private fun KeywordMiniSymbol(modifier: Modifier, keyword: GroupKeyword) {
 }
 
 @Composable
-fun PicFrontCardImage(
+private fun PicCroppedImageContainer(
     modifier: Modifier,
-    frontImageUrl: String,
     @DrawableRes frameResId: Int,
-    backgroundColor: Color = Gray0,
+    backgroundColor: Color,
+    content: @Composable BoxScope.() -> Unit,
 ) {
-    Box(modifier = modifier) {
-        AsyncImage(
-            modifier = Modifier.matchParentSize(),
-            model = frontImageUrl,
-            contentScale = ContentScale.Crop,
-            contentDescription = stringResource(R.string.group_main_picture),
-        )
+    Box(
+        modifier = modifier,
+    ) {
+        content()
         StableImage(
-            modifier = Modifier.matchParentSize(),
+            modifier = Modifier.fillMaxSize(),
             drawableResId = frameResId,
             colorFilter = ColorFilter.tint(backgroundColor),
             contentScale = ContentScale.FillBounds,
@@ -207,193 +147,43 @@ fun PicFrontCardImage(
 }
 
 @Composable
-fun PicBackCardImage(
+fun PicCroppedPhoto(
     modifier: Modifier,
-    cardBackImageList: List<CardBackImage>,
-    backgroundColor: Color,
+    imageUrl: String,
+    @DrawableRes frameResId: Int,
+    backgroundColor: Color = Gray0,
 ) {
-    LazyVerticalGrid(
-        modifier = modifier.wrapContentSize(),
-        verticalArrangement = Arrangement.spacedBy(7.46.dp),
-        horizontalArrangement = Arrangement.spacedBy(7.46.dp),
-        columns = GridCells.Fixed(2),
-        userScrollEnabled = false,
+    PicCroppedImageContainer(
+        modifier = modifier,
+        frameResId = frameResId,
+        backgroundColor = backgroundColor,
     ) {
-        items(cardBackImageList) { cardBackImage ->
-            Box(modifier = Modifier.aspectRatio(1f)) {
-                AsyncImage(
-                    modifier = Modifier.matchParentSize(),
-                    model = cardBackImage.imageUrl,
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null,
-                )
-                StableImage(
-                    modifier = Modifier.matchParentSize(),
-                    drawableResId = cardBackImage.frameType.frameResId,
-                    colorFilter = ColorFilter.tint(backgroundColor),
-                    contentScale = ContentScale.FillBounds,
-                    contentDescription = null,
-                )
-            }
-        }
+        AsyncImage(
+            modifier = Modifier.matchParentSize(),
+            model = imageUrl,
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(R.string.group_main_picture),
+        )
     }
 }
 
 @Preview
 @Composable
-private fun PicFrontPhotoCardPreview() {
-    PicPhotoCard(
-        modifier = Modifier.wrapContentSize(),
-        groupInfo = GroupInfo(
-            id = 1,
-            status = GroupStatusType.NO_PAST_AND_CURRENT_EVENT,
-            name = "Group Name",
-            keyword = GroupKeyword.CREW,
-            statusDescription = "status description",
-            cardFrontImageUrl = "https://picsum.photos/200/300",
-            frontImageFrame = PicPhotoFrame.HAMBURGER,
-            recentEventDate = "2021.10.10",
-            cardBackImages = listOf(
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.HAMBURGER,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.SNOWMAN,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.GHOST,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.PLUS,
-                ),
-            ),
-        ),
-        eventName = "이벤트 이름",
-        content = {
-            PicFrontCardImage(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 74.dp, bottom = 96.dp, start = 30.dp, end = 30.dp)
-                    .align(Alignment.Center),
-                frontImageUrl = "https://picsum.photos/200/300",
-                frameResId = PicPhotoFrame.HAMBURGER.frameResId,
-                backgroundColor = GroupKeyword.CREW.backgroundColor,
-            )
-        },
-    )
-}
-
-@Preview
-@Composable
-private fun PicBackPhotoCardPreview() {
-    PicPhotoCard(
-        modifier = Modifier.wrapContentSize(),
-        groupInfo = GroupInfo(
-            id = 1,
-            status = GroupStatusType.NO_PAST_AND_CURRENT_EVENT,
-            name = "Group Name",
-            keyword = GroupKeyword.CREW,
-            statusDescription = "status description",
-            cardFrontImageUrl = "https://picsum.photos/200/300",
-            frontImageFrame = PicPhotoFrame.HAMBURGER,
-            recentEventDate = "2021.10.10",
-            cardBackImages = listOf(
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.HAMBURGER,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.SNOWMAN,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.GHOST,
-                ),
-                CardBackImage(
-                    imageUrl = "https://picsum.photos/200/300",
-                    frameType = PicPhotoFrame.PLUS,
-                ),
-            ),
-        ),
-        eventName = "이벤트 이름",
-        content = {
-            PicBackCardImage(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 74.dp, bottom = 96.dp, start = 30.dp, end = 30.dp)
-                    .align(Alignment.Center),
-                cardBackImageList = listOf(
-                    CardBackImage(
-                        imageUrl = "https://picsum.photos/200/300",
-                        frameType = PicPhotoFrame.HAMBURGER,
-                    ),
-                    CardBackImage(
-                        imageUrl = "https://picsum.photos/200/300",
-                        frameType = PicPhotoFrame.SNOWMAN,
-                    ),
-                    CardBackImage(
-                        imageUrl = "https://picsum.photos/200/300",
-                        frameType = PicPhotoFrame.GHOST,
-                    ),
-                    CardBackImage(
-                        imageUrl = "https://picsum.photos/200/300",
-                        frameType = PicPhotoFrame.PLUS,
-                    ),
-                ),
-                backgroundColor = GroupKeyword.CREW.backgroundColor,
-            )
-        },
-    )
-}
-
-@Preview
-@Composable
-private fun PicFrontCardImagePreview() {
-    PicFrontCardImage(
+private fun PicCroppedPhotoPreview() {
+    PicCroppedPhoto(
         modifier = Modifier.fillMaxSize(),
         frameResId = PicPhotoFrame.PLUS.frameResId,
-        frontImageUrl = "https://picsum.photos/200/300",
+        imageUrl = "https://picsum.photos/200/300",
     )
 }
 
 @Preview
 @Composable
-private fun PicBackCardImagePreview() {
-    PicBackCardImage(
-        modifier = Modifier.wrapContentSize(),
-        cardBackImageList = listOf(
-            CardBackImage(
-                imageUrl = "https://picsum.photos/200/300",
-                frameType = PicPhotoFrame.HAMBURGER,
-            ),
-            CardBackImage(
-                imageUrl = "https://picsum.photos/200/300",
-                frameType = PicPhotoFrame.SNOWMAN,
-            ),
-            CardBackImage(
-                imageUrl = "https://picsum.photos/200/300",
-                frameType = PicPhotoFrame.GHOST,
-            ),
-            CardBackImage(
-                imageUrl = "https://picsum.photos/200/300",
-                frameType = PicPhotoFrame.PLUS,
-            ),
-        ),
-        backgroundColor = GroupKeyword.CREW.backgroundColor,
-    )
-}
-
-@Preview
-@Composable
-private fun PicPhotoCardContainerPreview() {
-    PicPhotoCardContainer(
+private fun PicPhotoCardFramePreview() {
+    PicPhotoCardFrame(
         modifier = Modifier.fillMaxSize(),
         keywordType = GroupKeyword.CREW,
+        frameResId = PicPhotoFrame.HAMBURGER.frameResId,
     ) {
         Box(
             modifier = Modifier
