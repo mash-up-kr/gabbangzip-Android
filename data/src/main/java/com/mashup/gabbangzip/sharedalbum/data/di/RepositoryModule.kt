fbc4a23d8c27c826d0838a34b0652fc5912399b2
seojeongwoo <qw4643987@naver.com>
2024-07-30 23:45:38 +0900

    :sparkles: 파이어베이스 관련 리포지토리 bind

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/di/RepositoryModule.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/di/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package com.mashup.gabbangzip.sharedalbum.data.di
 
 import com.mashup.gabbangzip.sharedalbum.data.repository.FileRepositoryImpl
+import com.mashup.gabbangzip.sharedalbum.data.repository.FirebaseRepositoryImpl
 import com.mashup.gabbangzip.sharedalbum.data.repository.GroupRepositoryImpl
 import com.mashup.gabbangzip.sharedalbum.data.repository.LoginRepositoryImpl
 import com.mashup.gabbangzip.sharedalbum.data.repository.UserRepositoryImpl
 import com.mashup.gabbangzip.sharedalbum.domain.repository.FileRepository
+import com.mashup.gabbangzip.sharedalbum.domain.repository.FirebaseRepository
 import com.mashup.gabbangzip.sharedalbum.domain.repository.GroupRepository
 import com.mashup.gabbangzip.sharedalbum.domain.repository.LoginRepository
 import com.mashup.gabbangzip.sharedalbum.domain.repository.UserRepository
@@ -32,4 +34,8 @@ interface RepositoryModule {
     @Singleton
     @Binds
     fun bindFileRepository(fileRepositoryImpl: FileRepositoryImpl): FileRepository
+
+    @Singleton
+    @Binds
+    fun bindFirebaseRepository(firebaseRepositoryImpl: FirebaseRepositoryImpl): FirebaseRepository
 }
