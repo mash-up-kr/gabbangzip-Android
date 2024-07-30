f2253bdf2f45f9c93746461baf192f298c576842
JeonK1 <kan5016@naver.com>
2024-07-30 20:37:50 +0900

    ✨ : contentType 추가

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/AwsService.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/AwsService.kt
@@ -2,6 +2,7 @@ package com.mashup.gabbangzip.sharedalbum.data.service
 
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.PUT
 import retrofit2.http.Part
@@ -11,6 +12,7 @@ interface AwsService {
     @Multipart
     @PUT
     suspend fun uploadFile(
+        @Header("Content-Type") contentType: String,
         @Url uploadUrl: String,
         @Part file: MultipartBody.Part,
     ): Response<Unit>
