879a3c4d29648c31cac81a0c1ca016562599acad
seojeongwoo <qw4643987@naver.com>
2024-08-07 16:15:00 +0900

    :sparkles: 투표 액티비티 생성

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteActivity.kt
@@ -5,16 +5,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class VoteActivity : ComponentActivity() {
-
+    private val viewModel: VoteViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val state by viewModel.voteUiState.collectAsStateWithLifecycle()
+
             PhotoVoteScreen(
-                onVoteFinish = { /*TODO*/ },
+                state = state,
+                onDialogConfirm = { viewModel.updateVoteDialog(isVisible = false) },
+                onCancelVote = { viewModel.updateVoteDialog(isVisible = true) },
+                onVoteExit = {
+                    MainActivity.openActivity(this@VoteActivity)
+                    finish()
+                },
+                onVoteBySwiped = { voteType, photo ->
+                    viewModel.updateVoteEvent(voteType)
+                    viewModel.addVoteResult(voteType, photo)
+                },
+                onVoteByClicked = { voteType, photo ->
+                    viewModel.addVoteResult(voteType, photo)
+                },
+                onVoteClick = { voteType ->
+                    viewModel.updateVoteEvent(voteType)
+                },
+                onSwipeFinish = { viewModel.finishVote() },
             )
         }
     }
