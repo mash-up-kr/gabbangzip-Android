820569c1adb2064818b6e2e3ce12e0c1c22bad7d
seojeongwoo <qw4643987@naver.com>
2024-08-07 16:16:03 +0900

    :sparkles: 투표 결과 리스트 저장,send 로직 구현

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteViewModel.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteViewModel.kt
@@ -1,19 +1,29 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.vote
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.gabbangzip.sharedalbum.domain.usecase.vote.RequestVoteResultUseCase
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.Photo
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteState
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteType
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.VoteResultUiParam
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.toDomainParam
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class VoteViewModel @Inject constructor() : ViewModel() {
+class VoteViewModel @Inject constructor(
+    private val requestVoteResultUseCase: RequestVoteResultUseCase,
+) : ViewModel() {
     private val _voteUiState = MutableStateFlow(PhotoVoteState())
     val voteUiState = _voteUiState.asStateFlow()
+
+    private val likedPhotoList = mutableListOf<Photo>()
 
     fun updateVoteEvent(type: PhotoVoteType) {
         val index = voteUiState.value.voteClickInfo.index + 1
@@ -37,10 +47,20 @@ class VoteViewModel @Inject constructor() : ViewModel() {
     }
 
     fun addVoteResult(voteType: PhotoVoteType, photo: Photo) {
-        // TODO: api 스펙 정의되면 리스트에 저장하는 로직 구현하기
+        if (voteType == PhotoVoteType.LIKE) {
+            likedPhotoList.add(photo)
+        }
     }
 
     fun finishVote() {
-        // TODO: api 스펙 정의되면 리스트를 서버에 보내는 로직 구현하기
+        viewModelScope.launch {
+            // TODO: 나중에 예외 처리 방법 결정되면 onSuccess,onFailure 구현하기
+            requestVoteResultUseCase(
+                VoteResultUiParam(
+                    eventId = voteUiState.value.eventId,
+                    voteResultIdList = ImmutableList(voteUiState.value.photoList.map { it.id }),
+                ).toDomainParam(),
+            )
+        }
     }
 }
