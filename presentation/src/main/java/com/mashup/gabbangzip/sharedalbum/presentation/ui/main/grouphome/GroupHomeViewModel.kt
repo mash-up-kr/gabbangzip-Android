9e648d2931cfe6e7af6d2215cf16cf9c48a6a5fa
Jihye Joo <oreocube13@gmail.com>
2024-08-11 00:07:03 +0900

    :sparkles: #141 GroupHomeViewModel 구현

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/GroupHomeViewModel.kt
@@ -0,0 +1,45 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.gabbangzip.sharedalbum.domain.usecase.group.GetGroupListUseCase
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.GroupHomeUiState
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.toUiModel
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupHomeViewModel @Inject constructor(
+    private val getGroupListUseCase: GetGroupListUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(GroupHomeUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getGroupList()
+    }
+
+    private fun getGroupList() {
+        _uiState.update { state ->
+            state.copy(
+                isLoading = true,
+            )
+        }
+        viewModelScope.launch {
+            getGroupListUseCase()
+                .onSuccess {
+                    _uiState.update { state ->
+                        state.copy(
+                            groupList = ImmutableList(it.toUiModel()),
+                            isLoading = false,
+                        )
+                    }
+                }
+        }
+    }
+}
