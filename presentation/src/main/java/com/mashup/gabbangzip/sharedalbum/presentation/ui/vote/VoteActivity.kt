fbee9e3f2b8e7797f4ef7d20e1a2d59ed5f0e430
seojeongwoo <qw4643987@naver.com>
2024-08-11 23:55:08 +0900

    :sparkles: naaHost 구현, 이동로직 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteActivity.kt
@@ -8,7 +8,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.rememberNavController
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.MainActivity
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.PicPhotoFrame
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.navigation.VoteNavHost
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.navigation.VoteNavRoute
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.navigation.navigateToVoteComplete
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,11 +21,16 @@ class VoteActivity : ComponentActivity() {
     private val viewModel: VoteViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.fetchUserName()
+
         setContent {
             val state by viewModel.voteUiState.collectAsStateWithLifecycle()
+            val navController = rememberNavController()
 
-            PhotoVoteScreen(
+            VoteNavHost(
                 state = state,
+                navController = navController,
+                startDestination = VoteNavRoute.initRoute,
                 onDialogConfirm = { viewModel.updateVoteDialog(isVisible = false) },
                 onCancelVote = { viewModel.updateVoteDialog(isVisible = true) },
                 onVoteExit = {
@@ -38,6 +48,17 @@ class VoteActivity : ComponentActivity() {
                     viewModel.updateVoteEvent(voteType)
                 },
                 onSwipeFinish = { viewModel.finishVote() },
+                onClickNavigationBack = { finish() },
+                onCompleteButtonClicked = { finish() },
+                onUploadPicture = { isUploadSuccess ->
+                    if (isUploadSuccess) {
+                        navController.navigateToVoteComplete()
+                    } else {
+                        // TODO: 향후 논의해서 업로드 실패시 에러토스트..? 스낵바..? 정하기.
+                    }
+                },
+                frameResId = PicPhotoFrame.getTypeByKeyword(state.voteResult.groupKeyword.name).frameResId,
+                thumbnailUrl = state.voteResult.randomImageUrl,
             )
         }
     }
