96c89c6b8073828c98d07143b1a305023596b900
seojeongwoo <qw4643987@naver.com>
2024-08-11 23:54:45 +0900

    :sparkles: userinfo 매퍼 추가 및 인자 수정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/UserInfo.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/model/UserInfo.kt
@@ -1,6 +1,9 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.model
 
+import com.mashup.gabbangzip.sharedalbum.domain.model.UserInfoDomainModel
+
 data class UserInfo(
-    val imageUrl: String = "",
     val name: String = "",
 )
+
+fun UserInfoDomainModel.toUiModel() = UserInfo(name = name)
