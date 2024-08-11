8ca6e5122fe4ca4355604c07a9000a2170947ede
seojeongwoo <qw4643987@naver.com>
2024-08-11 23:57:22 +0900

    :sparkles: 유저 네임 패치 함수 추가 및 업로드 성공 유무 업데이트 로직 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteViewModel.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/VoteViewModel.kt
@@ -2,12 +2,14 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.vote
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mashup.gabbangzip.sharedalbum.domain.model.vote.VoteResultParam
+import com.mashup.gabbangzip.sharedalbum.domain.usecase.LoadUserInfoUseCase
 import com.mashup.gabbangzip.sharedalbum.domain.usecase.vote.RequestVoteResultUseCase
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.toUiModel
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.Photo
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteState
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.PhotoVoteType
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.VoteResultUiParam
-import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.toDomainParam
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.vote.model.toUiModel
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,11 +21,21 @@ import javax.inject.Inject
 @HiltViewModel
 class VoteViewModel @Inject constructor(
     private val requestVoteResultUseCase: RequestVoteResultUseCase,
+    private val loadUserInfoUseCase: LoadUserInfoUseCase,
 ) : ViewModel() {
     private val _voteUiState = MutableStateFlow(PhotoVoteState())
     val voteUiState = _voteUiState.asStateFlow()
 
     private val likedPhotoList = mutableListOf<Photo>()
+
+    fun fetchUserName() {
+        viewModelScope.launch {
+            val userInfo = loadUserInfoUseCase()
+            _voteUiState.update { state ->
+                state.copy(userInfo = userInfo.toUiModel())
+            }
+        }
+    }
 
     fun updateVoteEvent(type: PhotoVoteType) {
         val index = voteUiState.value.voteClickInfo.index + 1
@@ -54,13 +66,25 @@ class VoteViewModel @Inject constructor(
 
     fun finishVote() {
         viewModelScope.launch {
-            // TODO: 나중에 예외 처리 방법 결정되면 onSuccess,onFailure 구현하기
             requestVoteResultUseCase(
-                VoteResultUiParam(
-                    eventId = voteUiState.value.eventId,
+                VoteResultParam(
+                    eventId = voteUiState.value.voteResult.eventId,
                     voteResultIdList = ImmutableList(voteUiState.value.photoList.map { it.id }),
-                ).toDomainParam(),
-            )
+                ),
+            ).onSuccess { voteResultDomain ->
+                _voteUiState.update { state ->
+                    state.copy(
+                        voteResult = voteResultDomain.toUiModel(),
+                        isVoteUploadFinish = true,
+                    )
+                }
+            }.onFailure {
+                _voteUiState.update { state ->
+                    state.copy(
+                        isVoteUploadFinish = false,
+                    )
+                }
+            }
         }
     }
 }
