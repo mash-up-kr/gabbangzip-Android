93429670b9caaa1e0a5f2a502bc44197640f582c
seojeongwoo <qw4643987@naver.com>
2024-08-13 11:35:55 +0900

    :sparkles: 투표할 이미지 리스트 가져오는 repository 구현

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/VoteRepositoryImpl.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/VoteRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.mashup.gabbangzip.sharedalbum.data.base.callApi
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.vote.toRequestBody
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.vote.toDomainModel
 import com.mashup.gabbangzip.sharedalbum.data.service.VoteService
+import com.mashup.gabbangzip.sharedalbum.domain.model.vote.VotePhotoDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.vote.VoteResultDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.vote.VoteResultParam
 import com.mashup.gabbangzip.sharedalbum.domain.repository.VoteRepository
@@ -15,6 +16,12 @@ class VoteRepositoryImpl @Inject constructor(
     override suspend fun requestVoteResult(param: VoteResultParam): VoteResultDomainModel {
         return callApi {
             voteService.requestVoteResult(param.toRequestBody())
+        }.toDomainModel()
+    }
+
+    override suspend fun getVotePhotoList(eventId: Long): List<VotePhotoDomainModel> {
+        return callApi {
+            voteService.getVotePhotoList(eventId)
         }.toDomainModel()
     }
 }
