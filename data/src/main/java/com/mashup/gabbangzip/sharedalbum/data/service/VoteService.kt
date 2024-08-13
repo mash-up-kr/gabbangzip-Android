b2d555a48509538b35391d7bdefcba3c6ae7c85f
seojeongwoo <qw4643987@naver.com>
2024-08-13 11:35:23 +0900

    :sparkles: 투표 이미지 get 하는 서비스 구현

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/VoteService.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/VoteService.kt
@@ -2,13 +2,21 @@ package com.mashup.gabbangzip.sharedalbum.data.service
 
 import com.mashup.gabbangzip.sharedalbum.data.base.PicResponse
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.vote.VoteResultRequest
+import com.mashup.gabbangzip.sharedalbum.data.dto.response.vote.VotePhotoResponse
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.vote.VoteResultResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface VoteService {
     @POST("api/v1/votes")
     suspend fun requestVoteResult(
         @Body request: VoteResultRequest,
     ): PicResponse<VoteResultResponse>
+
+    @GET("/api/v1/votes/{eventId}/options")
+    suspend fun getVotePhotoList(
+        @Path(value = "eventId") eventId: Long,
+    ): PicResponse<VotePhotoResponse>
 }
