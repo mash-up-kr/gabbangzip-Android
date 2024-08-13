acab09f0fd0f20f2c48035a38346cc898fede1f5
Jihye Joo <oreocube13@gmail.com>
2024-08-13 22:06:43 +0900

    :sparkles: #171 HistoryThumbnail 컴포저블 구현

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailHistory.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailHistory.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,8 +30,11 @@ import com.mashup.gabbangzip.sharedalbum.presentation.R
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray60
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.Gray80
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.PicTypography
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.common.PicCroppedPhoto
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail.model.HistoryItem
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail.preview.EventHistoryProvider
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.CardBackImage
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.StableImage
 import com.mashup.gabbangzip.sharedalbum.presentation.utils.noRippleClickable
 
@@ -118,11 +125,12 @@ private fun EventHistoryItemContainer(
                 onClickHistoryItem(item)
             },
     ) {
-        Box(
+        HistoryThumbnail(
             modifier = Modifier
                 .fillMaxWidth()
-                .aspectRatio(1f)
-                .background(color = Gray60),
+                .aspectRatio(1f),
+            backgroundColor = Gray60,
+            cardBackImageList = item.images,
         )
         Text(
             modifier = Modifier.padding(top = 6.dp),
@@ -136,6 +144,37 @@ private fun EventHistoryItemContainer(
             style = PicTypography.captionNormal12,
             color = Gray60,
         )
+    }
+}
+
+@Composable
+private fun HistoryThumbnail(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color,
+    cardBackImageList: ImmutableList<CardBackImage>,
+) {
+    Box(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(12.dp))
+            .background(color = backgroundColor)
+            .padding(10.dp),
+    ) {
+        LazyVerticalGrid(
+            modifier = Modifier.wrapContentSize(),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            columns = GridCells.Fixed(2),
+            userScrollEnabled = false,
+        ) {
+            items(items = cardBackImageList) { cardBackImage ->
+                PicCroppedPhoto(
+                    modifier = Modifier.aspectRatio(1f),
+                    imageUrl = cardBackImage.imageUrl,
+                    frameResId = cardBackImage.frameType.frameResId,
+                    backgroundColor = backgroundColor,
+                )
+            }
+        }
     }
 }
 
