71226df7526625b07e6742e7894f1862d02b9f6e
Jihye Joo <oreocube13@gmail.com>
2024-08-13 22:03:41 +0900

    :sparkles: HistoryItem 이미지 데이터 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/HistoryItem.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/HistoryItem.kt
@@ -1,6 +1,11 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail.model
 
+import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.CardBackImage
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.ImmutableList
+
 data class HistoryItem(
+    val id: Long = 0,
     val title: String,
     val date: String,
+    val images: ImmutableList<CardBackImage> = ImmutableList(emptyList()),
 )
