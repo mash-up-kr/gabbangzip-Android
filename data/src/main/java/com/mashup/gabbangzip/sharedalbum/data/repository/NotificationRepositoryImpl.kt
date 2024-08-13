d7f8ff027d802f5e19511335d07f3e6e132e4503
seojeongwoo <qw4643987@naver.com>
2024-08-13 15:06:33 +0900

    :sparkles: 쿡찌르기 repository 구현

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/NotificationRepositoryImpl.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/NotificationRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package com.mashup.gabbangzip.sharedalbum.data.repository
 
 import com.mashup.gabbangzip.sharedalbum.data.base.callApi
-import com.mashup.gabbangzip.sharedalbum.data.dto.request.toRequestBody
-import com.mashup.gabbangzip.sharedalbum.data.dto.response.toDomainModel
+import com.mashup.gabbangzip.sharedalbum.data.dto.request.notification.toRequestBody
+import com.mashup.gabbangzip.sharedalbum.data.dto.response.notification.toDomainModel
 import com.mashup.gabbangzip.sharedalbum.data.service.NotificationService
 import com.mashup.gabbangzip.sharedalbum.domain.model.notification.FcmTokenDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.notification.FcmTokenParamDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.notification.KookNotificationDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.notification.KookNotificationParamDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.repository.NotificationRepository
 import javax.inject.Inject
 
@@ -15,6 +17,13 @@ class NotificationRepositoryImpl @Inject constructor(
     override suspend fun registerFcmToken(token: FcmTokenParamDomainModel): FcmTokenDomainModel {
         val response = callApi {
             notificationService.registerToken(token.toRequestBody())
+        }
+        return response.toDomainModel()
+    }
+
+    override suspend fun sendKookNotification(kookParam: KookNotificationParamDomainModel): KookNotificationDomainModel {
+        val response = callApi {
+            notificationService.sendKookNotification(kookParam.toRequestBody())
         }
         return response.toDomainModel()
     }
--- domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/repository/NotificationRepository.kt
+++ domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/repository/NotificationRepository.kt
@@ -2,7 +2,10 @@ package com.mashup.gabbangzip.sharedalbum.domain.repository
 
 import com.mashup.gabbangzip.sharedalbum.domain.model.notification.FcmTokenDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.notification.FcmTokenParamDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.notification.KookNotificationDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.notification.KookNotificationParamDomainModel
 
 interface NotificationRepository {
     suspend fun registerFcmToken(token: FcmTokenParamDomainModel): FcmTokenDomainModel
+    suspend fun sendKookNotification(kookParam: KookNotificationParamDomainModel): KookNotificationDomainModel
 }
