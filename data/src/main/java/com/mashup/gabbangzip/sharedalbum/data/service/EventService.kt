0bd1c85b2efb0ad5cdfb1de6b5969a3bb6a07701
SEO-J17 <qw4643987@naver.com>
2024-08-16 02:28:22 +0900

    :sparkles: 첫번째 방문 체크 하는 api 추가

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/EventService.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/service/EventService.kt
@@ -5,8 +5,13 @@ import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.CreateEventReque
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.UploadMyPicRequest
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.CreateEventResponse
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.UploadMyPicResponse
+import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.CreateEventRequest
+import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.EventVisitRequest
+import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.CreateEventResponse
+import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.EventVisitResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface EventService {
     @POST("api/v1/events")
@@ -18,4 +23,9 @@ interface EventService {
     suspend fun uploadMyPic(
         @Body uploadMyPicRequest: UploadMyPicRequest,
     ): PicResponse<UploadMyPicResponse>
+
+    @PUT("api/v1/events/visit")
+    suspend fun checkVisitEvent(
+        @Body eventVisitRequest: EventVisitRequest,
+    ): PicResponse<EventVisitResponse>
 }
