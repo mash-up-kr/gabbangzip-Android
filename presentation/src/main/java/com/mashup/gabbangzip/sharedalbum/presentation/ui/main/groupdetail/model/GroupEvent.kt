7ad6bece271419fee8b8cdd0c8553929b2232fd6
Jihye Joo <oreocube13@gmail.com>
2024-08-16 00:36:37 +0900

    :sparkles: #189 그룹상세 날짜 형식 설정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/GroupEvent.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/model/GroupEvent.kt
@@ -1,6 +1,7 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail.model
 
 import com.mashup.gabbangzip.sharedalbum.domain.model.group.RecentEventDomainModel
+import com.mashup.gabbangzip.sharedalbum.presentation.utils.LocalDateUtil
 
 data class GroupEvent(
     val id: Long,
@@ -13,7 +14,9 @@ fun RecentEventDomainModel.toUiModel(): GroupEvent {
     return GroupEvent(
         id = id,
         title = title.orEmpty(),
-        date = date.toString(),
-        deadline = deadline?.toString().orEmpty(),
+        date = date?.let { LocalDateUtil.format(it) }.orEmpty(),
+        deadline = deadline?.let {
+            LocalDateUtil.format(it, "M월 d일 EEEE hh시 mm분") + " PIC 종료"
+        }.orEmpty(),
     )
 }
