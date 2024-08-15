0f67b5d1d6fb14a460fd86ac1b081855cc96b7f6
SEO-J17 <qw4643987@naver.com>
2024-08-16 02:28:42 +0900

    :sparkles: 방문 체크 로직 리포지토리에 추가

--- data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/EventRepositoryImpl.kt
+++ data/src/main/java/com/mashup/gabbangzip/sharedalbum/data/repository/EventRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.mashup.gabbangzip.sharedalbum.data.repository
 
 import com.mashup.gabbangzip.sharedalbum.data.base.callApi
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.CreateEventRequest
+import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.toRequestBody
+import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.toDomainModel
 import com.mashup.gabbangzip.sharedalbum.data.dto.request.event.UploadMyPicRequest
 import com.mashup.gabbangzip.sharedalbum.data.dto.response.event.toDomainModel
 import com.mashup.gabbangzip.sharedalbum.data.service.EventService
-import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventCreationDomainModel
-import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventCreationParam
+import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventVisitDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventVisitParamDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.eventcreation.EventCreationDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.eventcreation.EventCreationParam
 import com.mashup.gabbangzip.sharedalbum.domain.model.event.UploadMyPicDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.event.UploadMyPicParam
 import com.mashup.gabbangzip.sharedalbum.domain.repository.EventRepository
@@ -31,5 +35,9 @@ class EventRepositoryImpl @Inject constructor(
             imageUrls = param.imageUrls,
         )
         return callApi { eventService.uploadMyPic(request) }.toDomainModel()
+    }
+
+    override suspend fun checkVisitEvent(param: EventVisitParamDomainModel): EventVisitDomainModel {
+        return callApi { eventService.checkVisitEvent(param.toRequestBody()) }.toDomainModel()
     }
 }
--- domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/repository/EventRepository.kt
+++ domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/repository/EventRepository.kt
@@ -1,11 +1,14 @@
 package com.mashup.gabbangzip.sharedalbum.domain.repository
 
-import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventCreationDomainModel
-import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventCreationParam
 import com.mashup.gabbangzip.sharedalbum.domain.model.event.UploadMyPicDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.model.event.UploadMyPicParam
+import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventVisitDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.event.EventVisitParamDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.eventcreation.EventCreationDomainModel
+import com.mashup.gabbangzip.sharedalbum.domain.model.eventcreation.EventCreationParam
 
 interface EventRepository {
     suspend fun createEvent(param: EventCreationParam): EventCreationDomainModel
     suspend fun uploadMyPic(param: UploadMyPicParam): UploadMyPicDomainModel
+    suspend fun checkVisitEvent(param: EventVisitParamDomainModel): EventVisitDomainModel
 }
