12e8bc300971b13ce54a08717900f6417788da21
SEO-J17 <qw4643987@naver.com>
2024-08-16 15:21:51 +0900

    :sparkles: 투표 첫방문 여부 인자 추가

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/PhotoVoteState.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/vote/model/PhotoVoteState.kt
@@ -12,4 +12,5 @@ data class PhotoVoteState(
     val isLoading: Boolean = false,
     val isVoteUploadFinish: Boolean? = null,
     val isError: Boolean = false,
+    val isFirstVisit: Boolean = false,
 )
