91e1cf321543f02d2f2fa904c1bcbde6850f4a18
JeonK1 <kan5016@naver.com>
2024-08-17 23:13:19 +0900

    ✨ : 그룹생성화면 공통화 Scaffold 함수 생성

---
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/common/GroupCreationScaffold.kt
@@ -0,0 +1,44 @@
+package com.mashup.gabbangzip.sharedalbum.presentation.ui.groupcreation.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GroupCreationScaffold(
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment.Horizontal = Alignment.Start,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    content: @Composable ColumnScope.() -> Unit,
+    topBar: @Composable () -> Unit = {},
+    bottomFloatingButton: @Composable BoxScope.() -> Unit = {},
+) {
+    val scrollState = rememberScrollState()
+    Column(modifier = modifier) {
+        topBar()
+        Box(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding)
+                    .verticalScroll(scrollState),
+                horizontalAlignment = contentAlignment,
+                content = content,
+            )
+            Box(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                content = bottomFloatingButton
+            )
+        }
+    }
+}
