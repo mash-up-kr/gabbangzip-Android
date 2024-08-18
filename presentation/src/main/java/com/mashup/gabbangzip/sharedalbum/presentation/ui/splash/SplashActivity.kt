56e381907aa09c552ff88526c5d90ed64056b5f8
Jihye Joo <oreocube13@gmail.com>
2024-08-18 21:01:45 +0900

    :sparkles: #221 스플래시, 메인 statusbar 색상 제어

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/splash/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.mashup.gabbangzip.sharedalbum.presentation.ui.splash
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +24,14 @@ class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.dark(
+                TRANSPARENT,
+            )
+        )
         viewModel.checkUserLoggedIn()
         setContent {
             val state by viewModel.state.collectAsStateWithLifecycle()
