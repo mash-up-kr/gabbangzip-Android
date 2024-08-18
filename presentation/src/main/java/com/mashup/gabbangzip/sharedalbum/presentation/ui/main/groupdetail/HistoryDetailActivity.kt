223d881b414dd0b9a1d1c086e381294f4c84e971
Jihye Joo <oreocube13@gmail.com>
2024-08-18 20:37:01 +0900

    :sparkles: #199 statusbar 영역 색상 제어

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/HistoryDetailActivity.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/HistoryDetailActivity.kt
@@ -2,9 +2,12 @@ package com.mashup.gabbangzip.sharedalbum.presentation.ui.main.groupdetail
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.mashup.gabbangzip.sharedalbum.presentation.theme.SharedAlbumTheme
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupKeyword
 
@@ -13,6 +16,11 @@ class HistoryDetailActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                TRANSPARENT,
+            ),
+        )
         setContent {
             SharedAlbumTheme {
                 state?.let { state ->
