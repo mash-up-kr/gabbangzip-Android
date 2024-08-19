3f18a3bae7d3b15cb84a9f7ceeefded5bc37c934
Jihye Joo <oreocube13@gmail.com>
2024-08-19 23:18:23 +0900

    :sparkles: #234 그룹상세 재진입 시 리프레시

--- domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/usecase/group/GetGroupDetailUseCase.kt
+++ domain/src/main/java/com/mashup/gabbangzip/sharedalbum/domain/usecase/group/GetGroupDetailUseCase.kt
@@ -2,12 +2,16 @@ package com.mashup.gabbangzip.sharedalbum.domain.usecase.group
 
 import com.mashup.gabbangzip.sharedalbum.domain.model.group.GroupDomainModel
 import com.mashup.gabbangzip.sharedalbum.domain.repository.GroupRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetGroupDetailUseCase @Inject constructor(
     private val groupRepository: GroupRepository,
 ) {
-    suspend operator fun invoke(groupId: Long): Result<GroupDomainModel> {
-        return runCatching { groupRepository.getGroupDetail(groupId) }
+    operator fun invoke(groupId: Long): Flow<GroupDomainModel> {
+        return flow {
+            emit(groupRepository.getGroupDetail(groupId))
+        }
     }
 }
--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailViewModel.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/groupdetail/GroupDetailViewModel.kt
@@ -11,59 +11,34 @@ import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.grouphome.model.to
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.main.navigation.MainRoute
 import com.mashup.gabbangzip.sharedalbum.presentation.ui.model.GroupStatusType
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GroupDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getGroupDetailUseCase: GetGroupDetailUseCase,
+    getGroupDetailUseCase: GetGroupDetailUseCase,
     private val markEventVisitUseCase: MarkEventVisitUseCase,
 ) : ViewModel() {
-    private val groupId = savedStateHandle.get<Long>(MainRoute.GroupDetailRoute.KEY_GROUP_ID)
+    private val groupId = savedStateHandle.get<Long>(MainRoute.GroupDetailRoute.KEY_GROUP_ID) ?: 0
 
-    private val _uiState = MutableStateFlow(GroupDetailUiState())
-    val uiState = _uiState.asStateFlow()
-
-    init {
-        if (groupId != null) {
-            getGroupDetail(groupId)
-        } else {
-            _uiState.update { state ->
-                state.copy(isError = true)
-            }
-        }
-    }
-
-    private fun getGroupDetail(groupId: Long) {
-        _uiState.update { state ->
-            state.copy(isLoading = true)
-        }
-        viewModelScope.launch {
-            getGroupDetailUseCase(groupId)
-                .onSuccess { groupDetail ->
-                    _uiState.update { state ->
-                        state.copy(
-                            isLoading = false,
-                            groupInfo = groupDetail.toUiModel(),
-                            status = GroupStatusType.getType(groupDetail.status),
-                            recentEvent = groupDetail.recentEvent.toUiModel(),
-                            history = groupDetail.history.map { it.toUiModel() },
-                        )
-                    }
-                }.onFailure {
-                    _uiState.update { state ->
-                        state.copy(
-                            isLoading = false,
-                            isError = true,
-                        )
-                    }
-                }
-        }
-    }
+    val uiState = getGroupDetailUseCase(groupId)
+        .map { groupDetail ->
+            GroupDetailUiState(
+                isLoading = false,
+                groupInfo = groupDetail.toUiModel(),
+                status = GroupStatusType.getType(groupDetail.status),
+                recentEvent = groupDetail.recentEvent.toUiModel(),
+                history = groupDetail.history.map { it.toUiModel() },
+            )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = GroupDetailUiState(),
+        )
 
     fun markEventVisit() {
         uiState.value.recentEvent?.let { event ->
