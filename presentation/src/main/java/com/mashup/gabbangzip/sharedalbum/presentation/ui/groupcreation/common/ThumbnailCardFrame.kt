eeb35368dae568567cd1144035cb3580b3b2d940
JeonK1 <kan5016@naver.com>
2024-09-03 12:29:57 +0900

    :hammer: ktlint 오류 수정

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/common/ThumbnailCardFrame.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/groupcreation/common/ThumbnailCardFrame.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/GroupHomeScreen.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/main/grouphome/GroupHomeScreen.kt
@@ -43,7 +43,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
