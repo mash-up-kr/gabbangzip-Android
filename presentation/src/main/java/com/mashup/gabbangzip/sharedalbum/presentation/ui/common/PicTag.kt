ebbb992fdfa4032d7fb086ed6460a9536de8e786
JeonK1 <kan5016@naver.com>
2024-09-04 19:25:16 +0900

    :fire: delete unused import

--- presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicTag.kt
+++ presentation/src/main/java/com/mashup/gabbangzip/sharedalbum/presentation/ui/common/PicTag.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
